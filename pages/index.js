e11671b6c17a18cd997683ba42640dfd89872b04
Ulises Giacoman <ulises@joinhandshake.com>
2021-04-25 21:05:33 -0700

    Add dynamic tabla creation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,46 @@
 import styles from '../styles.module.css'
+import Image from 'next/image'
+
+
+export function Card(props) {
+  const width = 150
+  const height = 210  
+  const name = `/${props.name}.jpg`
+  return (
+    <Image
+        src={name}
+        alt="Gallo"
+        width={width}
+        height={height}
+        layout="fixed"
+      />
+  )
+}
 
 export default function Home() {
+  var count = 1
+  var fullDeck = []
+  
+  // Create deck of cards
+  while (fullDeck.length < 54) {
+    fullDeck.push(count)
+    count += 1
+  }
+
+  // Create tabla of 16 cards
+  var tabla = []
+  while (tabla.length < 16) {
+    const index = Math.floor(Math.random()*fullDeck.length)
+    const randomCard = fullDeck[index]
+    tabla.push(randomCard)
+    fullDeck.splice(index, 1)
+  }
+
   return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
+    <div className={styles.gridContainer}>
+    {tabla.map((item, index) => (
+      <Card key={index} name={item} />
+    ))}
     </div>
   )
 }
